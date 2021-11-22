5f2b73d717c384849f0cf8824e8bb197330a0c97
Jose Ramirez <sistemas@cersa.org.pe>
2021-11-21 21:31:46 -0500

    category

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -8,77 +8,106 @@
                 <div class="col-lg-12">
                   <h2 class="style-2">Explora nuestros cursos</h2>
                 </div>
-
-                <div class="row">
-                   <select v-model="selectedType">
-                    <option value="Ingeniería Eléctrica">Ingeniería Eléctrica</option>
-                     <option value="Ingeniería Civil">Ingeniería Civil</option>
-                    <option value="">Todas</option>
-                   
-                  </select>                  
-                 
-                
-                </div>
              
                 <div class="row">    
-                    <div class="search-wrapper  text-center">
-                      <input class="form-control" type="text" v-model="search" placeholder="Buscar curso o Diplomado"/>
-                  
-                    </div>               
-                  <template  v-for="t in filteredList">
-                    <div :key="t.id+'-top'"
-                      class="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-                      style="display: block; background-size: cover"
-                    >
-                      <div
-                        class="nft__item style-2"
-                        style="background-size: cover"
+                  <div class="col-md-12">
+                    <div class="row">
+                      <div class="col-md-7">
+                          <div class="search-wrapper  text-center">
+                          <input class="form-control" type="text" v-model="search" placeholder="Buscar curso o Diplomado"/>
+                           </div>  
+                      </div>
+                     <div class="col-md-2">
+                        <h4>Categorías</h4>
+                     </div>
+                      <div class="col-md-3">
+                      
+                         <select v-model="selectedType" class="form-control">
+                            
+                            <option value="Ingeniería Civil">Ingeniería Civil</option>
+                             <option value="Arquitectura">Arquitectura</option>
+                            <option value="Estructuras y Bim">Estructuras y Bim</option>
+                            <option value="Geología y Minería">Geología y Minería</option>
+                            <option value="Carreteras">Carreteras</option>
+                            <option value="Hidráulica, Hidrología y Riegos">Hidráulica, Hidrología y Riegos</option>
+                            <option value="Ingeniería Eléctrica">Ingeniería Eléctrica</option>
+                            <option value="Ambiental y Seguridad Ocupacional
+">Ambiental y Seguridad Ocupacional
+</option>
+                            <option value="Administración y Contabilidad">Administración y Contabilidad</option>
+                            <option value="Derecho">Derecho</option>
+                            <option value="Público en General">Público en General</option>
+                            <option value="Idiomas">Idiomas</option>
+                            <option value="Estudios Gubernamentales">Estudios Gubernamentales</option>                           
+                            <option value="
+Cursos Talleres Presenciales">
+Cursos Talleres Presencialesa</option>
+                            <option value="">Todas</option> 
+                          </select> 
+                      </div>
+                    </div>
+                  </div>
+                    
+                 
+                    <template  v-for="t in filteredList">
+                      <div :key="t.id+'-top'"
+                        class="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                        style="display: block; background-size: cover"
                       >
+                      <div class="author_list_pp" 
+                      style="background-size: cover; ">
+                      <p style="background: rgb(131,58,180);
+background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);" 
+class="border rounded text-white">15%</p>
+                      </div>
                         <div
-                          class="nft__item_wrap"
+                          class="nft__item style-2"
                           style="background-size: cover"
                         >
-                          <a href="03_grey-item-details.html">
-                            <img
-                              v-bind:src="'https://api.aulacersa.com:3006/images/cursos/'+t.imagen"
-                              class="lazy nft__item_preview"
-                              alt=""
-                            />
-                          </a>
-                        </div>
-                        <div
-                          class="nft__item_info"
-                          style="background-size: cover"
-                        >
-                          <a href="03_grey-item-details.html">
-                            <h4>{{t.nombre_corto}}</h4>
-                          </a>
                           <div
-                            class="nft__item_price"
+                            class="nft__item_wrap"
                             style="background-size: cover"
                           >
-                            <strike> S/ {{t.precio_carrito}} </strike
-                            ><span class="fs-2">S/ {{t.precio_carrito}}</span>
+                            <a href="03_grey-item-details.html">
+                              <img
+                                v-bind:src="'https://api.aulacersa.com:3006/images/cursos/'+t.imagen"
+                                class="lazy nft__item_preview"
+                                alt=""
+                              />
+                            </a>
                           </div>
                           <div
-                            class="nft__item_action"
+                            class="nft__item_info"
                             style="background-size: cover"
                           >
-                            <a href="#">COMPRAR</a>
-                          </div>
-                          <div
-                            class="nft__item_like"
-                            style="background-size: cover"
-                          >
-                            <i class="fa fa-heart"></i><span>80</span>
+                            <a href="03_grey-item-details.html">
+                              <h4>{{t.nombre_corto}}</h4>
+                            </a>
+                            <div
+                              class="nft__item_price"
+                              style="background-size: cover"
+                            >
+                              <strike> S/ {{t.precio_carrito}} </strike
+                              ><span class="fs-2">S/ {{Descuento(t.precio_carrito)}}</span>
+                            </div>
+                            <div
+                              class="nft__item_action"
+                              style="background-size: cover"
+                            >
+                              <a href="#">COMPRAR</a>
+                            </div>
+                            <div
+                              class="nft__item_like"
+                              style="background-size: cover"
+                            >
+                              <i class="fa fa-heart"></i><span>80</span>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </template>
-                  <!--Inicio del filer-->
-                  
-                  <!--fin de -->
+                    </template>
+               
+               
                  
                 </div>
           </div>
@@ -139,6 +168,10 @@ export default {
            
     
       },
+      Descuento(precio){
+        
+          return precio*0.85
+      },
        CursosTop(){
          let  contador_uno=0
         for (let i = 0;  i<8; i++) {
@@ -184,17 +217,7 @@ export default {
           return filtered
       })
     },
-   /* computed_items: function () {
-      let filterType= this.selectedType
-        //  filterSize = this.selectedSize
-      return this.cursos.filter(function(item){
-        let filtered = true
-        if(filterType && filterType.length > 0){
-         return  filtered = item.subcategoria.nombre == filterType
-        }
-         filtered
-      })
-    }*/
+ 
 
   }
 }
